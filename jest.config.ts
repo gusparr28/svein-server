f9bb348e3506515041d6eaa4a2cf5b9a7820cbf9
Gustavo Parra <gustavoparrabotero@gmail.com>
2022-07-21 16:36:23 -0400

    fixed roots property in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/tests'],
   verbose: true,
   bail: 1,
   preset: 'ts-jest',
